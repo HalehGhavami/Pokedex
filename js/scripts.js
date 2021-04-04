839a44a9640755abfe93b9b670cafe025f0fcf76
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2021-04-04 23:00:21 +0200

    Set up a first general dataset as an array

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
 
+let pokemonList[];
+  pokemonList = [
+    {
+    name :'Bulbasaur',
+    height : 0.7,
+    types : ['grass', 'poison']
+    },
+    {
+    name : 'Caterpie',
+    height : 0.3,
+    types : 'bug'
+    },
+    {
+    name : 'Pikachu',
+    height : 0.4,
+    types : 'electric'
+    }
+  ];
