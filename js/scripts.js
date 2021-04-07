d638e566598918a88417173b65063fdaa84b9899
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2021-04-07 21:38:32 +0200

    bolded specific printed text
    I tried to bold 'pokemonList.name' but it doesn't work

--- js/scripts.js
+++ js/scripts.js
@@ -27,7 +27,7 @@ for (let i = 0; i < pokemonList.length; i++) {
 
 //The conditional check if the height is above a certain value
   if (pokemonList[i].height > 0.4) {
-    document.write('- Wow, that\'s big!') ;
+    document.write('<b>- Wow, that\'s big!</b>') ;
   }
  document.write('</li><br>');
 }
