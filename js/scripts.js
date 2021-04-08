99b23622b4307c09dcbf47d07ab76ec58c6b5111
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2021-04-08 12:04:32 +0200

    Used jQuery to access specific part of unordered list for styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,20 @@
 // Creating an array of objects
-let pokemonList = [
-    {
-    name :'Bulbasaur',
-    height : 0.7,
-    types : ['grass', 'poison']
-    },
-    {
-    name : 'Caterpie',
-    height : 0.3,
-    types : ['bug']
-    },
-    {
-    name : 'Pikachu',
-    height : 0.4,
-    types : ['electric']
+let pokemonList = [{
+    name: 'Bulbasaur',
+    height: 0.7,
+    types: ['grass', 'poison']
+  },
+  {
+    name: 'Caterpie',
+    height: 0.3,
+    types: ['bug']
+  },
+  {
+    name: 'Pikachu',
+    height: 0.4,
+    types: ['electric']
   }
-  ];
+];
 
 // Display the data on the page as an unordered list
 
@@ -23,12 +22,19 @@ document.write('<ul class="pokemon-list">');
 
 for (let i = 0; i < pokemonList.length; i++) {
 
-  document.write('<li class="pokemon-list__item">'+ pokemonList[i].name + '(height:'+ pokemonList[i].height + ')');
 
-//The conditional check if the height is above a certain value
+  // Using jQuery to be able to make bold just the names of pokemon-list__items and styling <span> tag by css
+  document.write(`
+    <li class="pokemon-list__item">
+      <span>${pokemonList[i].name}</span> (height: ${pokemonList[i].height})
+  `);
+
+
+  //The conditional check if the height is above a certain value
+  // using <b> tag to bold part of a string display on page
   if (pokemonList[i].height > 0.4) {
-    document.write('<b>- Wow, that\'s big!</b>') ;
+    document.write('<b>- Wow, that\'s big!</b>');
   }
- document.write('</li><br>');
+  document.write('</li><br>');
 }
 document.write('<ul>');
