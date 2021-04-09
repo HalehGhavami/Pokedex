ec9f158952cdff5ac0eb9b5338d80c08a74068df
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2021-04-09 12:03:35 +0200

    Used + string concatenation instead of ${} (string interpolation)

--- js/scripts.js
+++ js/scripts.js
@@ -21,14 +21,15 @@ let pokemonList = [{
 document.write('<ul class="pokemon-list">');
 
 for (let i = 0; i < pokemonList.length; i++) {
+  //Used + string concatenation instead of ${} (string interpolation)
+  document.write('<li class="pokemon-list__item"><b>' + pokemonList[i].name + '</b> (height: ' + pokemonList[i].height + ')');
 
-
-  // Using jQuery to be able to make bold just the names of pokemon-list__items and styling <span> tag by css
-  document.write(`
-    <li class="pokemon-list__item">
-      <span>${pokemonList[i].name}</span> (height: ${pokemonList[i].height})
-  `);
-
+  /* Using jQuery to be able to make bold just the names of pokemon-list__items and styling <span> tag by css
+    document.write(`
+      <li class="pokemon-list__item">
+        <span>${pokemonList[i].name}</span> (height: ${pokemonList[i].height})
+    `);
+  */
 
   //The conditional check if the height is above a certain value
   // using <b> tag to bold part of a string display on page
