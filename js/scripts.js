a564d8e515a291572b5ad39ca39b64cf1e22a957
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2021-04-11 21:22:21 +0200

    commented where were used  "template string literal"
    ` ${}`

--- js/scripts.js
+++ js/scripts.js
@@ -21,15 +21,18 @@ let pokemonList = [{
 document.write('<ul class="pokemon-list">');
 
 for (let i = 0; i < pokemonList.length; i++) {
+
   //Used + string concatenation instead of ${} (string interpolation)
+
   document.write('<li class="pokemon-list__item"><b>' + pokemonList[i].name + '</b> (height: ' + pokemonList[i].height + ')');
 
-  /* Using jQuery to be able to make bold just the names of pokemon-list__items and styling <span> tag by css
-    document.write(`
-      <li class="pokemon-list__item">
-        <span>${pokemonList[i].name}</span> (height: ${pokemonList[i].height})
-    `);
-  */
+  /* Used this ECMAScript 6 feature that is named "template string literal" to be able to make bold just the names of pokemon-list__items and styling <span> tag by css*/
+
+  //  document.write(`
+  //  <li class="pokemon-list__item">
+  //  <span>${pokemonList[i].name}</span> (height: ${pokemonList[i].height})
+  //  `);
+
 
   //The conditional check if the height is above a certain value
   // using <b> tag to bold part of a string display on page
