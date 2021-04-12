43c036d881281aa9fac74d64152ae8a696c25c0c
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2021-04-12 22:23:09 +0200

    Indentation

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,52 @@
 //Wrapped pokemonList array in an IIFE to avoid accidentally accessing the global state.
 //create a new pokemonRepository variable to hold what my IIFE will return
-let pokemonRepository = (function(){
-// Creating an array of objects
-let pokemonList = [
-  {
-    name: 'Bulbasaur',
-    height: 0.7,
-    types: ['grass', 'poison']
-  },
-  {
-    name: 'Caterpie',
-    height: 0.3,
-    types: ['bug']
-  },
-  {
-    name: 'Pikachu',
-    height: 0.4,
-    types: ['electric']
+let pokemonRepository = (function() {
+  // Creating an array of objects
+  let pokemonList = [{
+      name: 'Bulbasaur',
+      height: 0.7,
+      types: ['grass', 'poison']
+    },
+    {
+      name: 'Caterpie',
+      height: 0.3,
+      types: ['bug']
+    },
+    {
+      name: 'Pikachu',
+      height: 0.4,
+      types: ['electric']
+    }
+  ];
+
+  //The IIFE return an object with the following public functions assigned as keys:
+
+  // add the Pokémon referred to with item to the pokemonList array +  datatype check
+  function add(pokemon) {
+    if (typeof pokemon === 'object' && typeof pokemon !== null) {
+      pokemonList.push(pokemon);
+    } else {
+      alert('type of parameter is not an object');
+    }
   }
-];
-
-//The IIFE return an object with the following public functions assigned as keys:
-
-// add the Pokémon referred to with item to the pokemonList array +  datatype check
-function add(pokemon) {
-  if (typeof pokemon === 'object' && typeof pokemon !== null ) {
-  pokemonList.push(pokemon);}
-  else{
-    alert('type of parameter is not an object');
+  //return the pokemonList array
+  function getAll() {
+    return pokemonList;
   }
-}
-//return the pokemonList array
-function getAll() {
-  return pokemonList;
-}
 
-return{
- getAll:getAll,
- add:add
- };
+  return {
+    getAll: getAll,
+    add: add
+  };
 
 })();
 
 //add the correct type of data to pokemonList array
-pokemonRepository.add({name: 'Raichu', height: 0.8,
-type: ['electric']});
+pokemonRepository.add({
+  name: 'Raichu',
+  height: 0.8,
+  type: ['electric']
+});
 
 // Display the data on the page as an unordered list
 document.write('<ul class="pokemon-list">');
