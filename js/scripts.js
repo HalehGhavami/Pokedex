ff658a0db4acbe40c9da297b793066e904728b7c
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2021-04-12 16:56:04 +0200

    move the function declaration passed to forEach() to make  clearer code

--- js/scripts.js
+++ js/scripts.js
@@ -18,28 +18,31 @@ let pokemonList = [{
 
 // Display the data on the page as an unordered list
 document.write('<ul class="pokemon-list">');
-//forEach Loop iterates each pokemon name  and height.
+
 //Used + string concatenation instead of ${} (string interpolation)
-pokemonList.forEach(function(pokemon) {
+
+//move the function declaration passed to forEach() to make things clearer
+
+function pokemonLoopFunction(pokemon) {
   document.write('<li class="pokemon-list__item"><b>' + pokemon.name + '</b> (height: ' + pokemon.height + ')');
 
+  /* Used this ECMAScript 6 feature that is named "template string literal" to be able to make bold just the names of pokemon-list__items and styling <span> tag by css
 
-
-  /* Used this ECMAScript 6 feature that is named "template string literal" to be able to make bold just the names of pokemon-list__items and styling <span> tag by css*/
-
-  //  document.write(`
-  //  <li class="pokemon-list__item">
-  //  <span>${pokemonList[i].name}</span> (height: ${pokemonList[i].height})
-  //  `);
-
+      document.write(`
+      <li class="pokemon-list__item">
+      <span>${pokemonList[i].name}</span> (height: ${pokemonList[i].height})
+     `);
+  */
 
   //The conditional check if the height is above a certain value
   // using <b> tag to bold part of a string display on page
-
-
   if (pokemon.height > 0.4) {
     document.write('<b>- Wow, that\'s big!</b>');
   }
   document.write('</li><br>');
-});
+};
+
+//forEach Loop iterates each pokemon name and height.
+pokemonList.forEach(pokemonLoopFunction);
+
 document.write('<ul>');
