6cc1e9f2f1be849503bb2ddb01eb0d1e9d827b79
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2021-04-14 16:11:24 +0200

    Manipulated the DOM. Added list item and a button for each displayed pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
-//Wrapped pokemonList array in an IIFE to avoid accidentally accessing the global state.
+//Wrapped repository array in an IIFE to avoid accidentally accessing the global state.
 //create a new pokemonRepository variable to hold what my IIFE will return
 let pokemonRepository = (function() {
-  let pokemonList = [{
+  let repository = [{
       name: 'Bulbasaur',
       height: 0.7,
       types: ['grass', 'poison']
@@ -20,47 +20,51 @@ let pokemonRepository = (function() {
 
   //The IIFE return an object with the following public functions assigned as keys:
 
-  // add the Pokémon referred to with item to the pokemonList array +  datatype check
+  // add the Pokémon referred to with item to the repository array +  datatype check
   function add(pokemon) {
     if (typeof pokemon === 'object' && typeof pokemon !== null) {
-      pokemonList.push(pokemon);
+      repository.push(pokemon);
     } else {
       alert('type of parameter is not an object');
     }
   }
 
-  //return the pokemonList array
+  //return the repository array
   function getAll() {
-    return pokemonList;
+    return repository;
+  }
+
+  //creating lists and button in the DOM
+  function addListItem(pokemon) {
+    let pokemonList = document.querySelector('.pokemon-list');
+    let listItem = document.createElement('li');
+
+    //pokemon names on the buttons
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('button-class');
+
+    //append the butten und the list to thier parents
+    listItem.appendChild(button);
+    pokemonList.appendChild(listItem);
   }
 
   return {
     getAll: getAll,
-    add: add
+    add: add,
+    addListItem: addListItem,
   };
 
 })();
 
-//add the correct type of data to pokemonList array
+//add the correct type of data to repository array
 pokemonRepository.add({
   name: 'Raichu',
   height: 0.8,
   types: ['electric']
 });
 
-document.write('<ul class="pokemon-list">');
-//move the function declaration passed to forEach() to make things clearer
-function myLoopFunction(pokemon) {
-  document.write('<li class="pokemon-list__item"><b>' + pokemon.name + '</b> (height: ' + pokemon.height + ')' + "<br>" + pokemon.types);
 
-  document.write('</li><br>');
-};
-
-//forEach Loop iterates each pokemon name and height.
-pokemonRepository.getAll().forEach(myLoopFunction);
-
-
-
-
-
-document.write('<ul>');
+//forEach Loop iterates each pokemon name in a button in an unorderd list
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addListItem(pokemon);});
