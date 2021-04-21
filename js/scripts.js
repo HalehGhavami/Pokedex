d926888e6d619c2b9ae46b038b65ac381651a55b
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2021-04-21 13:49:32 +0200

    edited event listener to close by click
    We only want to close if the user clicks directly on the overlay

--- js/scripts.js
+++ js/scripts.js
@@ -152,11 +152,14 @@ let pokemonRepository = (function() {
   });
 
   modalContainer.addEventListener('click', function(e) {
+    // Since this is also triggered when clicking INSIDE the modal
+    // We only want to close if the user clicks directly on the overlay
     let target = e.target;
-    if (target = modalContainer) {
+    if (target === modalContainer) {
       hideModal()
     }
   });
+
 
   return {
     getAll: getAll,
